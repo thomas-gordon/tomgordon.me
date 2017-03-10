9924b8025468d41c1400764c722b74846ef6ea86
Tom Gordon <tom.gordon@squareweave.com.au>
2017-03-10 14:54:59 +1100

    Edit some copy, mod readme to be more relevant.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -61,17 +61,20 @@ class HomePage extends React.PureComponent { // eslint-disable-line react/prefer
           <h1>Hi, I′m Tom. </h1>
           <h2>I create with the internet to try and make the world a better place.
           </h2>
-          <p>I call myself a Frontend developer, as my focus and interest in development is on excellent user experience
-            delivered through
-            forward thinking technology. I aim to deliver a given digital platform across the myriad platforms
-            used to consume the given digital platform. Starting simply with HTML templates, my skillset and ability has
-            expanded to the point where I can create whole apps by myself, from design through to deployment..</p>
+          <p>I call myself a Frontend developer because my focus and interest in development is on the user experience
+            delivered through forward thinking, speedy technology. I always aim to deliver a given digital project along with a high degree of compatibility
+            across the myriad platforms used to consume the web. Having had 10 years experience, I am able to adjust the delivery of a project according to the
+            relevant <a href="https://en.wikipedia.org/wiki/Project_management_triangle">time/cost/quality</a> paradigm.</p>
+          <p>I began my development career with simple HTML template cutup and since then my skillset and ability has
+            expanded to the point where I can deploy whole responsive React apps by myself, from design through to deployment.</p>
+          <p>I am well versed in Agile development, having worked on projects that land all along the spectrum from complete waterfall to Agile SCRUM.</p>
           <h3>Currently at work I′m using the following tools to get the job done.</h3>
           <ul>
             <li>Tech Libraries: <a href="https://facebook.github.io/react/" target="_blank">React</a>, <a href="https://facebook.github.io/react-native/" target="_blank">React Native</a>, <a href="https://www.djangoproject.com/" target="_blank">Django</a>, <a href="https://wordpress.com" target="_blank">Wordpress</a></li>
             <li>Build tools: <a href="https://docs.npmjs.com/misc/scripts" target="_blank">NPM Scripts</a>, <a href="https://webpack.github.io/" target="_blank">webpack</a>, <a href="http://gulpjs.com/" target="_blank">gulp</a>, <a href="https://gruntjs.com/" target="_blank">grunt</a></li>
             <li>Testing: <a href="https://facebook.github.io/jest/" target="_blank">Jest</a>, <a href="https://www.browserstack.com/" target="_blank">BrowserStack</a></li>
             <li>Analytics: <a href="https://analytics.google.com/analytics/web/" target="_blank">Google Analytics</a>, <a href="https://mixpanel.com/" target="_blank">Mixpanel</a></li>
+            <li>Project Management: <a href="https://www.atlassian.com/software/jira" target="_blank">Atlassian Jira</a></li>
             <li>Wireframing: <a href="https://www.axure.com/" target="_blank">Axure</a>, <a href="https://www.invisionapp.com/" target="_blank">InVision</a></li>
             <li>Design: <a href="http://www.adobe.com/au/creativecloud.html" target="_blank">Adobe CC Suite</a></li>
             <li>Animation: <a href="https://greensock.com/" target="_blank">GreenSock</a></li>
