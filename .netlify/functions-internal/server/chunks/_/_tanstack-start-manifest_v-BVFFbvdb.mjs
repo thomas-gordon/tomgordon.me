901d557afbbb0740be092370f1aea24ad5aaf641
Tom Gordon <thomas.gordon@artprocessors.net>
2025-07-08 15:15:31 +1000

    chore: Remove tanstack cruft

--- .netlify/functions-internal/server/chunks/_/_tanstack-start-manifest_v-BVFFbvdb.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "/Users/tomgordon/sites/tomgordon.me/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-BfYxaktz.js"], "assets": [] }, "/": { "filePath": "/Users/tomgordon/sites/tomgordon.me/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-B7GZPGBj.js"] } }, "clientEntry": "/assets/main-BfYxaktz.js" });
-
-export { tsrStartManifest };
-//# sourceMappingURL=_tanstack-start-manifest_v-BVFFbvdb.mjs.map
